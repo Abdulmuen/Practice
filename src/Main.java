9be9fac1dc596ed31786bce8d8290e8fc38a4d6c
Abdulmuen <mabdulmuen@gmail.com>
2023-04-29 06:53:11 -0700

    Version 2.0

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,58 @@ public class Main {
         final int MONTHS = 12;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
 
-        double principal = scanner.nextDouble();
-        System.out.print("Annual Interest Rate: ");
+        double principal;
+        double monthlyInterestRate;
+        int numberOfPayments;
 
-        double monthlyInterestRate = (scanner.nextDouble() / PERCENT) / MONTHS;
-        System.out.print("Period (Years): ");
 
-        int numberOfPayments = scanner.nextInt() * MONTHS;
+        while(true){
+            System.out.print("Principal ($1K - $1M: ");
+            principal = scanner.nextDouble();
+            if(principal >= 1_000 && principal <= 1_000_000)
+              break;
+            System.out.println("Enter a number between 1,000 ans 1,000,000");
+        }
+
+
+
+        while (true){
+            System.out.print("Annual Interest Rate: ");
+            monthlyInterestRate = scanner.nextDouble();
+            if(monthlyInterestRate > 0 && monthlyInterestRate <= 30){
+                monthlyInterestRate = (monthlyInterestRate / PERCENT) / MONTHS;
+                break;
+            }
+            System.out.println("Enter a value greater than 0 and less than or equal to 30");
+        }
+
+
+
+        while (true){
+            System.out.print("Period (Years): ");
+            numberOfPayments = scanner.nextInt();
+            if(numberOfPayments >= 1 && numberOfPayments <= 30){
+                numberOfPayments *=  MONTHS;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         System.out.println("Mortgage: " + currency.format(mortgage(principal,monthlyInterestRate,numberOfPayments)));
     }
 
     public static double mortgage(double principal, double monthlyInterestRate, int numberOfPayment) {
         return principal * ((monthlyInterestRate*Math.pow((1+monthlyInterestRate),numberOfPayment))/(Math.pow((1+monthlyInterestRate),numberOfPayment)-1));
+    }
+    public static void fizzBuzz(int number){
+        if(number%3==0 || number%5==0){
+            if(number%3==0 && number%5 == 0){
+                System.out.println("FizzBuzz");
+            } else if (number%3==0) {
+                System.out.println("Buzz");
+            }else System.out.println("Fizz");
+        }else
+            System.out.println(number);
     }
 }
